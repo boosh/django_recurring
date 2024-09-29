3e16117526fef6354ee63518f958c8a3c69cd7e6
boosh <boosh@example.com>
2024-09-29 17:41:36 +0100

    Rename non-recursive funcs

--- src/recurring/utils.py
+++ src/recurring/utils.py
@@ -1,4 +1,7 @@
-def camel_to_snake(name):
+import re
+
+
+def _camel_to_snake(name):
     """
     Converts a single camelCase string to snake_case.
     """
@@ -12,7 +15,7 @@ def recursive_camel_to_snake(data):
     if isinstance(data, dict):
         new_data = {}
         for key, value in data.items():
-            new_key = camel_to_snake(key)
+            new_key = _camel_to_snake(key)
             new_data[new_key] = recursive_camel_to_snake(value)  # Recursively apply to values
         return new_data
     elif isinstance(data, list):
@@ -21,9 +24,8 @@ def recursive_camel_to_snake(data):
         return data  # If it's neither dict nor list, return it as is
 
 
-import re
 
-def snake_to_camel(name):
+def _snake_to_camel(name):
     """
     Converts a single snake_case string to lowerCamelCase.
     """
@@ -37,7 +39,7 @@ def recursive_snake_to_camel(data):
     if isinstance(data, dict):
         new_data = {}
         for key, value in data.items():
-            new_key = snake_to_camel(key)
+            new_key = _snake_to_camel(key)
             new_data[new_key] = recursive_snake_to_camel(value)  # Recursively apply to values
         return new_data
     elif isinstance(data, list):
